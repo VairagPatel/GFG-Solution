32aacbdd30e7635bd23e2dca0e376b7643cd2044
Vairag Akbari <vairag.techwork@gmail.com>
2025-08-04 16:43:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        // code here
+        int m=mat.length;
+        int n=mat[0].length;
+
+        int row[]=new int [m];
+        int col[]=new int[n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1||col[j]==1){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        
+    }
+}
